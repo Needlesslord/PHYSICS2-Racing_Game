ecaab57a6fe5eb01e71895434080dcb78be0352f
tomascarreras1000 <tomascarreras1000@gmail.com>
2019-12-23 23:13:08 +0100

    New Copia to work on because fuck merges

---
+++ Elemental Race - The Game - Copy/ModulePlayer.h
@@ -0,0 +1,97 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "p2Point.h"
+#include "PhysVehicle3D.h"
+#include "Timer.h"
+
+
+struct PhysVehicle3D;
+
+#define MAX_ACCELERATION 1000.0f
+#define TURN_DEGREES 15.0f * DEGTORAD
+#define BRAKE_POWER 1000.0f
+#define MAX_SPEED 200
+#define MIN_SPEED -50
+
+struct Checkpoint {
+
+	float sizeX;
+	float sizeY;
+	float sizeZ;
+
+	float posX_left;
+	float posY_left;
+	float posZ_left;
+
+	float posX_right;
+	float posY_right;
+	float posZ_right;
+
+	float posX_respawn_point;
+	float posY_respawn_point;
+	float posZ_respawn_point;
+	
+	int lap;
+	bool activated;
+	bool activated_last;
+	int colour;			// 0 for red, not activated; 1 for orange, activated 1; 2 for yellow, activated 2; 3 for green, activated 3
+
+	bool isStart = false;
+	int num_checkpoint;
+
+};
+
+class ModulePlayer : public Module {
+public:
+	ModulePlayer(Application* app, bool start_enabled = true);
+	virtual ~ModulePlayer();
+
+	bool Start();
+	update_status Update(float dt);
+	void OnCollision(PhysBody3D* body1, PhysBody3D* body2);
+	bool CleanUp();
+
+public:
+	VehicleInfo car;
+	VehicleInfo bus;
+	VehicleInfo bike;
+	VehicleInfo truck;
+	VehicleInfo trailer;
+	VehicleInfo racingCar;
+	VehicleInfo monsterTruck;
+	VehicleInfo bigTruck;
+	VehicleInfo bigTrailer;
+	VehicleInfo mini;
+
+	/*PhysVehicle3D* Car;
+	PhysVehicle3D* Bus;
+	PhysVehicle3D* Bike;
+	PhysVehicle3D* Truck;
+	PhysVehicle3D* RacingCar;
+	PhysVehicle3D* MonsterTruck;
+	PhysVehicle3D* BigTruck;
+	PhysVehicle3D* BigTrailer;
+	PhysVehicle3D* Mini;*/
+
+	PhysVehicle3D* Player;
+	PhysVehicle3D* Trailer;
+	//STRUCT VEHICLE
+	
+	bool vehicleSelected;
+	uint vehicleSelectedNum; // 0 -> car; 1 -> bus; 2 -> truck; 3 -> racingCar; 4 -> mosterTruck; 5 -> bigTruck; 6 -> mini; T -> add Trailer 
+	bool trailerAdded;
+
+	float turn;
+	float acceleration;
+	float brake;
+
+	bool following_camera;
+	vec3 dist_to_car;
+
+	//timer
+	Timer			timer;
+	bool			timerOn = false;
+	bool			hasWon = false;
+	bool			raceFinished = false;
+};
---
+++ Elemental Race - The Game - Copy/ModuleSceneIntro.h
@@ -0,0 +1,112 @@
+#pragma once
+#include "Module.h"
+#include "p2DynArray.h"
+#include "Globals.h"
+#include "Primitive.h"
+
+#define MAX_SNAKE 2
+
+struct PhysBody3D;
+struct PhysMotor3D;
+
+struct cubePieces
+{
+	p2DynArray<PhysBody3D*>		phys_bodies;
+	p2DynArray<Cube>			prim_bodies;
+};
+
+enum Steps {
+	SelectVehicle,
+	LockVehicle,
+	Running,
+	GameOver
+};
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update(float dt);
+	bool CleanUp();
+	void OnCollision(PhysBody3D* body1, PhysBody3D* body2);
+	uint course_index = 0;
+	Cube CreateObject_BorderCourse(float posX, float posY, float posZ);
+	uint Mario_size;
+	bool CreateCourse(float list[], uint size);
+	Cube* Course[440]; //MIGHT NEED TO BE BIGGER
+
+	void AddCheckpoint(float posX, float posY, float posZ);
+	bool CreateCheckpoints(float list[], uint size);
+	uint checkpoint_index = 0;
+	uint checkpoints_list_size;
+	p2DynArray<PhysBody3D*>		Checkpoints;
+	Cube* checkpoint_cubes[3];
+	uint checkpointToActivate;
+	int checkpointActivated;
+
+	//to add all the obstacles
+	void AddObstacles(int zone);
+	void AddBridge();
+
+	Cube* up;
+	Cube* flat;
+	Cube* down;
+	void AddPerson(float posX, float posY, float posZ, int ethnicity);
+	Cube AddHead(int ethnicity);
+	Cube AddBody(int shirt);
+	Cube* water_lake;
+	PhysBody3D* start_line;
+	Cube* start_line_cube;
+	PhysBody3D* mid_line;
+	Cube* mid_line_cube;
+	bool startActivated;
+	bool midActivated;
+	uint lap;
+
+	//fire archs
+	Cube* left_1;
+	Cube* top_1;
+	Cube* right_1;
+	Cube* left_2;
+	Cube* top_2;
+	Cube* right_2;
+	Cube* left_3;
+	Cube* top_3;
+	Cube* right_3;
+	Cube* left_4;
+	Cube* top_4;
+	Cube* right_4;
+
+	//bushes
+	Cube* bush_1;
+	Cube* bush_2;
+	Cube* bush_3;
+	Cube* bush_4;
+	Cube* bush_5;
+	Cube* bush_6;
+	Cube* bush_7;
+	Cube* bush_8;
+	Cube* bush_9;
+	Cube* bush_10;
+	Cube* bush_11;
+	Cube* bush_12;
+	Cube* bush_13;
+	Cube* bush_14;
+	Cube* bush_15;
+
+
+	void AddFireArch();
+	void AddBush();
+
+	Steps currentStep;
+public:
+	//obstacles list
+	int num_trees;
+	int num_rocks;
+	int num_fire_archs;
+	int num_fireballs;
+	int num_people;
+
+};
